411cddc172400a7faac975548e1c059f6d2f4c42
Tommaso Turchi <apps@tommasoturchi.fyi>
2021-08-17 15:52:41 +0200

    updated graphics + auto pagination of publications

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ function render(resume) {
   Handlebars.registerHelper("idFromType", function (type) {
     return type.split(" ")[0].toLocaleLowerCase();
   });
-  
+
+  Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
+    return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+  });
+
   Handlebars.registerHelper("expandCountryCode", function (countryCode) {
     switch (countryCode) {
       case "UK":
